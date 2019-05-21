121857b97e13cce2b5927446c384e36a6e8220f8
Dean Wood <dean@deans-imac.home>
2019-05-21 15:05:16 +0100

    added a test file for the input tests to go. Largely to remind myself how to use GIT.

---
+++ test/test_input.py
@@ -0,0 +1,9 @@
+import pytest
+
+""" Tests for classlist
+First test is for input. 
+Expects comma delimited list input for class list.
+Expects another comma delimited list for constraints with keywords 'with' and 'not with' denoting choices 
+"""
+
+
